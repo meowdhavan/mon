9c63817a7372d0b9733e402154cad99a17b1f22a
madhavan-raja <madhavanraja99@gmail.com>
2026-06-10 11:08:06 -0700

    refactor: make Moon'm members public for easier initialization

--- moon.go
+++ moon.go
@@ -3,30 +3,16 @@ package moon
 import "os"
 
 type Moon struct {
-	rootCmd *Command
-	printer Printer
+	RootCmd *Command
+	Printer Printer
 }
 
-type moonOption func(*Moon)
+func NewMoon(rootCmd *Command) *Moon {
+	p := NewDefaultPrinter(os.Stdout, false)
 
-func WithPrinter(p Printer) moonOption {
-	return func(m *Moon) {
-		m.printer = p
-	}
-}
-
-func NewMoon(rootCmd *Command, options ...moonOption) *Moon {
 	m := &Moon{
-		rootCmd: rootCmd,
-	}
-
-	for _, opt:= range options {
-		opt(m)
-	}
-
-	if m.printer == nil {
-		p := NewDefaultPrinter(os.Stdout, false)
-		m.printer = &p
+		RootCmd: rootCmd,
+		Printer: &p,
 	}
 
 	return m
@@ -35,25 +21,25 @@ func NewMoon(rootCmd *Command, options ...moonOption) *Moon {
 func (m *Moon) Execute() {
 	showHelp := false
 
-	m.rootCmd.BoolFlag(&showHelp, "help", "h", "Show help message")
+	m.RootCmd.BoolFlag(&showHelp, "help", "h", "Show help message")
 
-	p := newParser(m.rootCmd, os.Args)
+	p := newParser(m.RootCmd, os.Args)
 	p.parse()
 
 	cmd := p.currentCmd
 
 	if !p.unrecognizedSubcommand && (showHelp || cmd.Run == nil) {
-		m.printer.printHelp(cmd)
+		m.Printer.printHelp(cmd)
 		os.Exit(0)
 	}
 
 	if len(p.errors) > 0 {
-		m.printer.printFullUsage(cmd, &p.errors, &p.warnings)
+		m.Printer.printFullUsage(cmd, &p.errors, &p.warnings)
 		os.Exit(3)
 	}
 
 	if len(p.warnings) > 0 {
-		m.printer.printWarnings(&p.warnings)
+		m.Printer.printWarnings(&p.warnings)
 	}
 
 	cmd.Run()
